ba2ce5539ac2f9a5b2e823cb10fd4ddbdf5f0210
keewonf <lukasfmm@gmail.com>
2025-03-02 00:34:37 -0300

    changeUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://moano-moano-app.9yolqh.easypanel.host'
+  baseURL: 'https://moano-project-igniteapi.9yolqh.easypanel.host'
 })
